e497bf1c8204c16257460796c7b89a9181cb1b79
Jay Francis <jay@whistlingmedia.com>
2014-05-18 00:53:18 +0100

    Generate docblocks

--- src/Generator.php
+++ src/Generator.php
@@ -14,9 +14,14 @@ use Zend\Code\Generator\FileGenerator;
 use Zend\Code\Generator\MethodGenerator;
 use Zend\Code\Generator\ParameterGenerator;
 use Phalcon\Annotations\Adapter\Memory as AnnotationReader;
+use Zend\Code\Generator\DocBlock\Tag\GenericTag as DocBlockTag;
 
 class Generator
 {
+  // Action description
+  const DOC_ACTION_DESCRIPTION = 'description';
+  const DOC_ACTION_METHOD_PARAM = 'methodParam';
+
   private $sdk;
   private $outputDir;
   private $indentation;
@@ -130,15 +135,177 @@ class Generator
     CollectionRoute $route
   )
   {
-    // Build the class name
     $className = $this->getRequestClassName($collection, $route);
     $this->printMessage(sprintf('Generating Request: %s', $className));
+
+    // Get the class docblock
+    $docBlock = $this->getRequestClassDocBlock($collection, $route);
+
+    echo $docBlock->generate();
 
     return $this;
   }
 
   /**
+   * Get the DockBlock for a request class
+   *
+   * @param Collection      $collection
+   * @param CollectionRoute $route
+   *
+   * @return DocBlockGenerator
+   */
+  private function getRequestClassDocBlock(
+    Collection $collection,
+    CollectionRoute $route
+  )
+  {
+    $docBlock = new DocBlockGenerator();
+
+    // Description
+    $description = $this->getActionAnnotation(
+      $collection,
+      $route,
+      self::DOC_ACTION_DESCRIPTION
+    );
+    if(!$description)
+    {
+      $description = sprintf(
+        "Please use annotation @%s('Details...')",
+        self::DOC_ACTION_DESCRIPTION
+      );
+    }
+    $docBlock->setShortDescription($description);
+
+    // Method (Class) params
+    $methodParams = $this->getActionMethodParams($collection, $route);
+    if($methodParams)
+    {
+      $docBlock->setTags($methodParams);
+    }
+
+    return $docBlock;
+  }
+
+  /**
+   * Get Action Method Params
+   *
+   * @param Collection      $collection
+   * @param CollectionRoute $route
+   *
+   * @return DocBlockTag[]|bool
+   */
+  private function getActionMethodParams(
+    Collection $collection,
+    CollectionRoute $route
+  )
+  {
+    $methodParams = $this->getActionAnnotations(
+      $collection,
+      $route,
+      self::DOC_ACTION_METHOD_PARAM
+    );
+
+    if(!$methodParams)
+    {
+      return false;
+    }
+
+    $tags = [];
+    foreach($methodParams as $paramData)
+    {
+      // todo recognise types here
+      $paramName = $paramData;
+      $paramType = "string";
+
+      $tag = new DocBlockTag();
+      $tag->setName('param');
+      $tag->setContent(sprintf('$%s %s', $paramName, $paramType));
+
+      $tags[] = $tag;
+    }
+
+    return $tags;
+  }
+
+  /**
+   * Get an array of action annotations by type
+   *
+   * @param Collection      $collection
+   * @param CollectionRoute $route
+   * @param                 $annotationType
+   *
+   * @return array|bool
+   */
+  private function getActionAnnotations(
+    Collection $collection,
+    CollectionRoute $route,
+    $annotationType
+  )
+  {
+    $classReader = $this->getClassAnnotationReader($collection->controller);
+    $methodParams = $classReader->getMethodsAnnotations();
+
+    if(!isset($methodParams[$route->controllerAction]))
+    {
+      return false;
+    }
+
+    try
+    {
+      return $methodParams[$route->controllerAction]
+        ->get($annotationType)
+        ->getArguments();
+    }
+    catch(\Exception $e)
+    {
+      return false;
+    }
+  }
+
+  /**
+   * Get an action annotation
+   *
+   * @param Collection      $collection
+   * @param CollectionRoute $route
+   * @param                 $annotationType
+   *
+   * @return string|bool
+   */
+  private function getActionAnnotation(
+    Collection $collection,
+    CollectionRoute $route,
+    $annotationType
+  )
+  {
+    $annotations = $this->getActionAnnotations(
+      $collection,
+      $route,
+      $annotationType
+    );
+
+    if(!$annotations)
+    {
+      return false;
+    }
+
+    return $annotations[0];
+  }
+
+  /**
+   * Get Annotation Reader for class
+   *
+   * @param $class
+   *
+   * @return \Phalcon\Annotations\Reflection
+   */
+  private function getClassAnnotationReader($class)
+  {
+    return (new AnnotationReader())->get($class);
+  }
+
+  /**
    * Get the request class name for a controller action
+   *
    * @param Collection      $collection
    * @param CollectionRoute $route
    */
@@ -151,7 +318,7 @@ class Generator
     // Get the controller class name
     $controller = new $collection->controller;
     $controllerReflection = new \ReflectionClass($controller);
-    $controllerClassName =  $controllerReflection->getShortName();
+    $controllerClassName = $controllerReflection->getShortName();
     $controllerClassName = str_replace('Controller', '', $controllerClassName);
 
     return sprintf(
