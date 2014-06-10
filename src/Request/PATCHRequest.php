a436c4e6b6cc0313906cb1907fc9981402b7ce23
Agnieszka <agnieszka@wzukltd.com>
2014-06-10 16:31:45 +0100

    PATCH Request update method

--- src/Request/PATCHRequest.php
+++ src/Request/PATCHRequest.php
@@ -5,8 +5,11 @@ namespace PhrestSDK\Request;
 
 class PATCHRequest extends Request
 {
+  protected $path;
+  protected $options = null;
+
   public function update()
   {
-    // todo
+    return parent::getResponse(self::METHOD_PATCH, $this->path, $this->options);
   }
 }
