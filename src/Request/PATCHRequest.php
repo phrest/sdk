566e6a2d1b3dab723e22656f7ccbf08cf997cdb8
Agnieszka <agnieszka@wzukltd.com>
2014-06-23 17:55:41 +0100

    request params issue - patch hack

--- src/Request/PATCHRequest.php
+++ src/Request/PATCHRequest.php
@@ -5,11 +5,11 @@ namespace PhrestSDK\Request;
 
 class PATCHRequest extends Request
 {
-  protected $path;
-  protected $options = null;
+ // protected $path;
+ // protected $options = null;
 
-  public function update()
+  public function update($path, $options) //dirty hack to make patch work //todo fix this
   {
-    return parent::getResponse(self::METHOD_PATCH, $this->path, $this->options);
+    return parent::getResponse(self::METHOD_PATCH, $path, $options);
   }
 }
