03e5d4277c6bbf95215b92ced53a65b046eaaa38
Seb Pollard <seb@spolster.co.uk>
2014-08-06 11:34:04 +0100

    Make options optional.

--- src/Request/DELETERequest.php
+++ src/Request/DELETERequest.php
@@ -5,7 +5,7 @@ namespace PhrestSDK\Request;
 
 class DELETERequest extends Request
 {
-  public static function deleteByPath($path, RequestOptions $options)
+  public static function deleteByPath($path, RequestOptions $options = null)
   {
     return parent::getResponse(self::METHOD_DELETE, $path, $options);
   }
